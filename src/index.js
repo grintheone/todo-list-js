4113d02a21effca9259ca1e5e76afeb3a0d6a7a7
Alex <grin3run@gmail.com>
2021-01-11 14:49:11 +0300

    Barebone design

--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
 import { fromUnixTime } from "date-fns";
-import './style/style.css'
-import {todo} from './logic.js'
+import './style/style.css';
+import {todo} from './logic.js';
 import {appendToTheDom} from './dom.js';
 
-
+var list = document.querySelectorAll('ul');
+for (let i = 0; i < list.length; i++) {
+    list[i].addEventListener('click', function(ev) {
+        if (ev.target.tagName === 'LI') {
+           ev.target.classList.toggle('done');
+        }
+      }, false);
+}
